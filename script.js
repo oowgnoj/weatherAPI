042afc0fb9233413b63a1fceec614757aa573ba7
김화영 <dalle0601@gmail.com>
2019-08-24 11:23:12 +0900

    temp, cityname

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ function selectChange() {
     document.getElementById("city").selectedIndex
   ].value;
   //   console.log(opt);
-  return opt;
+  getWeathr(opt);
 }
+
+const API_KEY = "1a159477d8d8169aedfbe3c51575dc17";
+function getWeathr(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperatur = json.main.temp;
+      const place = json.name;
+      console.log(temperatur, place);
+    });
+}
+
+getWeathr("seoul");
